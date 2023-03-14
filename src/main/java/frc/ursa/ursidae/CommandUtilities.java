e7547b7f5e293e3abad76e23107384ed91f50a4b
teamursamajor <teamursamajor@gmail.com>
2023-03-14 16:37:29 -0400

    Refractor

--- src/main/java/frc/ursa/ursidae/CommandUtilities.java
+++ src/main/java/frc/ursa/ursidae/CommandUtilities.java
@@ -1,4 +1,4 @@
-package frc.ursa.ursidae.command;
+package frc.ursa.ursidae;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
--- src/main/java/frc/ursa/ursidae/Logger.java
+++ src/main/java/frc/ursa/ursidae/Logger.java
@@ -1,7 +1,6 @@
 package frc.ursa.ursidae;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.ursa.ursidae.command.CommandUtilities;
 /**
  * Logging utilities
  */
